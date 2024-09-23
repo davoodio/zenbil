b2ba7aed8df918d7f8ce283908f44a25c055aa06
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-23 20:24:56 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/splash/splash_model.dart
+++ lib/pages/splash/splash_model.dart
@@ -3,11 +3,6 @@ import 'splash_widget.dart' show SplashWidget;
 import 'package:flutter/material.dart';
 
 class SplashModel extends FlutterFlowModel<SplashWidget> {
-  ///  State fields for stateful widgets in this page.
-
-  // Stores action output result for [Custom Action - handleDynamicLink] action in Splash widget.
-  dynamic actionFinished;
-
   @override
   void initState(BuildContext context) {}
 
